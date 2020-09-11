de0b7efc314935d95ac99b4a9973d50d4b9bdb47
Youssef Eddibili <eddibili.youssef@gmail.com>
2020-09-11 17:08:23 +0200

    tmp

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { hot } from "react-hot-loader";
+import { z } from "react-hot-loader";
 
 const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
-
+dfad;
 class App extends React.Component<Record<string, unknown>, undefined> {
   public render() {
     return (
