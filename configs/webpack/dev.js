5f3e94ee2c382e93f3cb71fcf489e6f85d2c3089
Youssef Eddibili <eddibili.youssef@gmail.com>
2020-09-27 21:37:56 +0200

    webpack dev server : ssl support

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -2,18 +2,23 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const commonConfig = require('./common');
+const fs = require('fs');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
   entry: [
     'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
     './index.tsx', // the entry point of our app
   ],
   devServer: {
     host: '0.0.0.0',
-    hot: true, // enable HMR on the server
+    port: 8080,
+    hot: true, // enable HMR on the server,
+    https: {
+      cert: fs.readFileSync('/Users/eddib/.localhost-ssl/localhost.crt'),
+      key: fs.readFileSync('/Users/eddib/.localhost-ssl/localhost.key'),
+    },
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
